9aa799cf8da03852bd8a0cb7dcdca1714a7a7a13
VICTOR HUGO INACIO DE OLIVEIRA <victor.oliveira@petronect.com.br>
2025-06-10 17:10:06 -0300

    feat: rota adicionada

--- src/serve.ts
+++ src/serve.ts
@@ -18,7 +18,7 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
